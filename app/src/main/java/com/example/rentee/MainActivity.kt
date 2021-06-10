13862b5d223c0e52b365db059f92ba6d77544c5b
Eliram Elgrably <33867061+EliramElgrably@users.noreply.github.com>
2021-06-10 19:20:24 +0300

    Fixing some issues with the address fragment

--- app/src/main/java/com/example/rentee/MainActivity.kt
+++ app/src/main/java/com/example/rentee/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.rentee.databinding.ActivityMainBinding
 
@@ -23,15 +22,19 @@ class MainActivity : AppCompatActivity() {
         // The layout for this activity is a Data Binding layout so it needs to be inflated using
         // DataBindingUtil.
         binding = DataBindingUtil.setContentView(
-            this, R.layout.activity_main)
+            this, R.layout.activity_main
+        )
 
         navController = findNavController(R.id.nav_host_fragment)
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
     }
 
--- app/src/main/java/com/example/rentee/ui/home/NewRentAddressFragment.kt
+++ app/src/main/java/com/example/rentee/ui/home/NewRentAddressFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.rentee.R
 import com.example.rentee.databinding.FragmentNewRentAddressBinding
 import com.example.rentee.databinding.FragmentNewRentBinding
 
@@ -27,6 +30,14 @@ class NewRentAddressFragment : Fragment() {
         // LiveData needs the lifecycle owner
         binding.lifecycleOwner = this
 
+        binding.ivClose.setOnClickListener {
+            navigateToHomePage()
+        }
+
         return binding.root
+    }
+
+    private fun navigateToHomePage() {
+       findNavController().navigateUp()
     }
 }
